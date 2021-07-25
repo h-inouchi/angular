9d007fb8e7f1f12388aee0fa6cc5a5aedc830c86
h-inouchi <hibird2720804@gmail.com>
2021-07-25 23:19:51 +0900

    76 end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+import { User } from './class/user';
+
+const CURRENT_USER:User = new User(1, '井内　はじめ');
+const ANOTHER_USER:User = new User(2, '竹井　賢治');
+
+const COMMENTS: Comment[] = [
+  new Comment(ANOTHER_USER, 'お疲れ様です！'),
+  new Comment(ANOTHER_USER, 'このあいだの件ですが、どうなりましたか？'),
+  new Comment(CURRENT_USER, 'あーあれですか'),
+  new Comment(CURRENT_USER, 'OKでました'),
+]
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMMENTS;
+  currentUser = CURRENT_USER;
+  comment = '';
+
+  addComment(comment: string): void {
+    if (comment) {
+      this.comments.push(new Comment(this.currentUser, comment));
+    }
+  }
 }
