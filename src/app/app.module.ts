697cdddd4a6a4c4f48d1d9001b8c9cc558a06ca7
h-inouchi <hibird2720804@gmail.com>
2021-08-21 21:07:05 +0900

    module分割

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,23 @@
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
-import { FormsModule } from '@angular/forms';
+
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CommentDatePipe } from './pipes/comment-date.pipe';
-import { environment } from '../environments/environment';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 import { ChatComponent } from './chat/chat.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentDatePipe,
     ChatComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AngularFireModule.initializeApp(
-      environment.firebase
-    ),
-    AngularFireAuthModule,
-    AngularFireDatabaseModule,
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
