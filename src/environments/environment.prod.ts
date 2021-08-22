821c728de525ba44b8eeee20ed7b587dcea96e9e
h-inouchi <hibird2720804@gmail.com>
2021-08-22 15:37:42 +0900

    setting prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA422o-8uQ8XFXHAE1UYthbdUj2pF-cqX0",
+    authDomain: "angular-chat-16593.firebaseapp.com",
+    projectId: "angular-chat-16593",
+    storageBucket: "angular-chat-16593.appspot.com",
+    messagingSenderId: "180551004242",
+    appId: "1:180551004242:web:60cae859c3297190f26f8b"
+  }
 };
